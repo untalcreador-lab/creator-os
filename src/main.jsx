61e6f3381594955fb239d3cbf049bfbf8497e6a0
untalcreador-lab <untalcreador@gmail.com>
2026-02-19 01:44:16 +0000

    Reparar inicio de app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  document.body.innerHTML = '<div style="color:red; padding:20px;">Error Crítico: No encuentro el div root</div>';
+} else {
+  try {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  } catch (error) {
+    document.body.innerHTML = '<div style="color:white; padding:20px;"><h1>Error de Inicio</h1><pre>' + error.message + '</pre></div>';
+  }
+}
