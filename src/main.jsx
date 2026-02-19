12c807d7ac89f5409aae1efce67ec253a5a2012c
untalcreador-lab <untalcreador@gmail.com>
2026-02-19 01:48:15 +0000

    Reinicio nuclear de archivos

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  document.body.innerHTML = '<div style="color:red; padding:20px;">Error Crítico: No encuentro el div root</div>';
-} else {
-  try {
-    ReactDOM.createRoot(rootElement).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-  } catch (error) {
-    document.body.innerHTML = '<div style="color:white; padding:20px;"><h1>Error de Inicio</h1><pre>' + error.message + '</pre></div>';
-  }
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
